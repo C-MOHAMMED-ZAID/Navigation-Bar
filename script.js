3261ca27cdcdc3e29a2a48196ad35797a227f0a4
ZAID <mohammedzaid.connect@gmail.com>
2024-10-08 10:41:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    
+    // Toggle the 'show' class to show/hide the menu
+    navLinks.classList.toggle('show');
+}
