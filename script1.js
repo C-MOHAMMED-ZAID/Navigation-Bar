523fb9045c3aeabab210edeef0932f7ea2f38290
ZAID <mohammedzaid.connect@gmail.com>
2024-10-08 10:41:09 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    
-    // Toggle the 'show' class to show/hide the menu
-    navLinks.classList.toggle('show');
-}
